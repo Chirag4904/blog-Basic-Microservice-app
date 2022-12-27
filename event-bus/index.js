e474d30e126682ce118622954dd9ec60fffc1ca2
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-27 12:20:36 +0530

    emit comment updated event

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,11 +10,21 @@ app.use(cors());
 app.post("/events", async (req, res) => {
 	const event = req.body;
 
-	await axios.post("http://localhost:4000/events", event);
+	await axios.post("http://localhost:4000/events", event).catch((err) => {
+		console.log(err.message, "4000");
+	});
 
-	await axios.post("http://localhost:4001/events", event);
+	await axios.post("http://localhost:4001/events", event).catch((err) => {
+		console.log(err.message, "4001");
+	});
 
-	await axios.post("http://localhost:4002/posts", event);
+	await axios.post("http://localhost:4002/posts", event).catch((err) => {
+		console.log(err.message, "4002");
+	});
+
+	await axios.post("http://localhost:4003/events", event).catch((err) => {
+		console.log(err.message, "4003");
+	});
 	res.send({ status: "OK" });
 });
 
