ab1f92a6cdf99993976f6aa40cae76ea46d0fc10
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-29 14:44:07 +0530

    changing endpoint

--- posts/index.js
+++ posts/index.js
@@ -14,7 +14,7 @@ app.get("/posts", (req, res) => {
 	res.send(posts);
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
 	const id = randomBytes(4).toString("hex");
 
 	const { title } = req.body;
@@ -44,5 +44,5 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-	console.log("Listening on port 4000");
+	console.log("Listening on porrt 4000");
 });
